6d3817d321473cacc634a2acff0398da43747522
jloh-foreflight <jloh@foreflight.com>
2023-08-02 19:04:07 -0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { LoginCallback, SecureRoute } from '@okta/okta-react';
 import { Home } from './pages/Home';
 import { NavbarComp } from './components/NavbarComp';
 import Container from 'react-bootstrap/Container';
-import Test from './pages/Test';
+// import Test from './pages/Test';
 
 
 function App() {
@@ -13,8 +13,8 @@ function App() {
       <NavbarComp />
       <Container className="mb-4 ml-4">
         {/* <Switch> */}
-          <SecureRoute path="/" exact={true} component={Test}/>
-          <SecureRoute path="/home" component={Home} />
+          {/* <SecureRoute path="/" exact={true} component={Test}/> */}
+          <SecureRoute path="/" component={Home} />
           {/* <SecureRoute path="/converter" component={Converter} /> */}
           <Route path="/login/callback" component={LoginCallback} />
         {/* </Switch> */}
